a125e6ad2a2a8a78d316225688a8e9553aebbaa9
zhangyu1wx <56&78Pdz>
2023-06-27 22:18:26 +0800

    new

--- sf/main.java
+++ sf/main.java
@@ -13,56 +13,107 @@ public static void main(String[]args){
         return new int[0];
     }
 
-    // 删除有序数组中的重复项
-    //给你一个有序数组 nums ，请你 原地 删除重复出现的元素，使每个元素 只出现一次 ，返回删除后数组的新长度。
-    //不要使用额外的数组空间，你必须在 原地 修改输入数组 并在使用 O(1) 额外空间的条件下完成
-    public int removeDuplicates(int[] nums) {
-        int n = nums.length;
-        if (n == 0) {
-            return 0;
+    // 最小操作次数使数组元素相等
+    // 给你一个长度为 n 的整数数组，每次操作将会使 n - 1 个元素增加 1 。返回让数组所有元素相等的最小操作次数。
+    public int minMoves(int[] nums) {
+        int minNum = Arrays.stream(nums).min().getAsInt();
+        int res = 0;
+        for (int num : nums) {
+            res += num - minNum;
         }
-        int fast = 1, slow = 1;
-        while (fast < n) {
-            if (nums[fast] != nums[fast - 1]) {
-                nums[slow] = nums[fast];
-                ++slow;
+        return res;
+    }
+    // 非递减数列
+    // 给你一个长度为 n 的整数数组，请你判断在 最多 改变 1 个元素的情况下，该数组能否变成一个非递减数列
+    public boolean checkPossibility(int[] nums) {
+        int n = nums.length, cnt = 0;
+        for (int i = 0; i < n - 1; ++i) {
+            int x = nums[i], y = nums[i + 1];
+            if (x > y) {
+                cnt++;
+                if (cnt > 1) {
+                    return false;
+                }
+                if (i > 0 && y < nums[i - 1]) {
+                    nums[i + 1] = x;
+                }
             }
-            ++fast;
         }
-        return slow;
+        return true;
+    }
+    // 移动零
+    // 给定一个数组 nums，编写一个函数将所有 0 移动到数组的末尾，同时保持非零元素的相对顺序
+    public void moveZeroes(int[] nums) {
+        int n = nums.length, left = 0, right = 0;
+        while (right < n) {
+            if (nums[right] != 0) {
+                swap(nums, left, right);
+                left++;
+            }
+            right++;
+        }
     }
 
-    // 请实现一个函数，把字符串 s 中的每个空格替换成"%20"
-    public String replaceSpace(String s) {
-        int length = s.length();
-        char[] array = new char[length * 3];
-        int size = 0;
-        for (int i = 0; i < length; i++) {
-            char c = s.charAt(i);
-            if (c == ' ') {
-                array[size++] = '%';
-                array[size++] = '2';
-                array[size++] = '0';
-            } else {
-                array[size++] = c;
-            }
-        }
-        String newStr = new String(array, 0, size);
-        return newStr;
+    public void swap(int[] nums, int left, int right) {
+        int temp = nums[left];
+        nums[left] = nums[right];
+        nums[right] = temp;
     }
-    // 输入一个链表的头节点，从尾到头反过来返回每个节点的值（用数组返回）。
-    public int[] reversePrint(ListNode head) {
-        Stack<ListNode> stack = new Stack<ListNode>();
-        ListNode temp = head;
-        while (temp != null) {
-            stack.push(temp);
-            temp = temp.next;
+
+    // 轮转数组
+    // 给定一个数组，将数组中的元素向右移动 k 个位置，其中 k 是非负数
+    public void rotate(int[] nums, int k) {
+        int n = nums.length;
+        k = k % n;
+        int count = gcd(k, n);
+        for (int start = 0; start < count; ++start) {
+            int current = start;
+            int prev = nums[start];
+            do {
+                int next = (current + k) % n;
+                int temp = nums[next];
+                nums[next] = prev;
+                prev = temp;
+                current = next;
+            } while (start != current);
         }
-        int size = stack.size();
-        int[] print = new int[size];
-        for (int i = 0; i < size; i++) {
-            print[i] = stack.pop().val;
+    }
+
+    public int gcd(int x, int y) {
+        return y > 0 ? gcd(y, x % y) : x;
+    }
+
+    // 解法二
+    public void rotate(int[] nums, int k) {
+        k %= nums.length;
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, nums.length - 1);
+    }
+
+    public void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start += 1;
+            end -= 1;
         }
-        return print;
+    }
+    // 旋转函数
+    // 给定一个长度为 n 的整数数组 A 。假设 Bk 是数组 A 顺时针旋转 k 个位置后的数组，我们定义 A 的“旋转函数” F 为：
+    // F(k) = 0 * Bk[0] + 1 * Bk[1] + ... + (n-1) * Bk[n-1]
+    // 输入: nums = [4,3,2,6]
+    public int maxRotateFunction(int[] nums) {
+        int f = 0, n = nums.length, numSum = Arrays.stream(nums).sum();
+        for (int i = 0; i < n; i++) {
+            f += i * nums[i];
+        }
+        int res = f;
+        for (int i = n - 1; i > 0; i--) {
+            f += numSum - n * nums[i];
+            res = Math.max(res, f);
+        }
+        return res;
     }
 }
